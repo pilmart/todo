1c1effc61f2615f326cfabc73ef8c8b124f3363b
Martin Scullion <martin.scullion@cgi.com>
2025-06-05 16:13:55 +0100

    Revisions

--- todomodule/todo.go
+++ todomodule/todo.go
@@ -3,56 +3,104 @@ package main
 // imports
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math/rand/v2"
 	"os"
 	"strconv"
+	"strings"
 )
 
+// struct used to maintain an individual todo item
+
 func main() {
+
+	// Declare variable for our new todo item
+	var toDo ToDo
+	// Declare array of new todo items
+	var toDos []ToDo
+
+	/*
+	 Initial flag setup to capture new todo information
+	 id - generate random int simulate db id generation
+	 description - default to empty string
+	 status - not started
+	*/
+
+	toDo.Id = rand.IntN(100000) + 1
+	fmt.Printf("%d", toDo.Id)
+
+	flag.StringVar(&toDo.Description, "description", " ", "Description of to do item")
+	flag.StringVar(&toDo.Status, "status", "not started", "Status of to do item")
+	flag.Parse()
+
+	fmt.Printf("%+v\n", toDo)
+
+	toDos = append(toDos, toDo)
+	fmt.Printf(" todos array %+v\n", toDos)
+
+	var todos = loadAll()
+
+	var sb strings.Builder
+
+	for i := 0; i < len(todos.ToDos); i++ {
+		sb.WriteString("id : " + strconv.Itoa(todos.ToDos[i].Id) + " ")
+		sb.WriteString("description : " + todos.ToDos[i].Description + " ")
+		sb.WriteString("status : " + todos.ToDos[i].Status + "\n")
+	}
+
+	fmt.Println("Data/todos.json " + sb.String())
+
+}
+
+func loadAll() ToDos {
+
+	log.Println("Starting loadAll")
+
+	// is of type ToDos
+	var todos ToDos
 
 	todoFile, err := os.Open("./data/todos.json")
 	// if we returns an error then handle it
 	if err != nil {
 		log.Fatalf("Unable to open file : %s", err)
 	}
-	fmt.Println("Successfully Opened users.json")
+	fmt.Println("Successfully Opened todos.json")
 
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer todoFile.Close()
 
+	// Read the file in....
 	byteValue, err := io.ReadAll(todoFile)
 	if err != nil {
-		log.Fatalf("Unable to ReadAll :  %s", err)
+		log.Fatalf("loadAll - Unable to ReadAll :  %s", err)
 	}
-	fmt.Println("Successfully Read bytevalue")
-	fmt.Printf("%s", byteValue)
+	log.Println("Successfully Read bytevalue")
 
-	var todos ToDos
-
+	// marshal the json...
 	err = json.Unmarshal(byteValue, &todos)
 	if err != nil {
 		log.Fatalf("Unable to marshal JSON due to %s", err)
 	}
-	fmt.Println("length " + strconv.Itoa(len(todos.ToDos)))
-
-	for i := 0; i < len(todos.ToDos); i++ {
-		fmt.Println("id : " + strconv.Itoa(todos.ToDos[i].Id))
-		fmt.Println("description : " + todos.ToDos[i].Description)
-		fmt.Println("status : " + todos.ToDos[i].Status)
-	}
+	log.Println("To Do's contains " + strconv.Itoa(len(todos.ToDos)) + " items ")
+	// hand back our result..
+	return todos
 }
 
-// struct used to maintain an individual todo item
+func saveAll() {
+
+}
+
+// ToDo Struct outlining a simple to do item
 type ToDo struct {
 	Id          int    `json:"id"`
 	Description string `json:"description"`
 	Status      string `json:"status"`
 }
 
-// ToDo struct which contains
-// an array of users
+// ToDos struct which contains array of ToDo items
 type ToDos struct {
 	ToDos []ToDo `json:"todos"`
 }
